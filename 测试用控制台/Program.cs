aa7bff7d01280efd3a841b30d29f3807e6019574
hjc2000 <hjc.666@foxmail.com>
2023-05-22 00:03:35 +0800

    使用HttpClient接收Chunk编码的数据成功

--- 测试用控制台/Program.cs
+++ 测试用控制台/Program.cs
@@ -1,9 +1,19 @@
 ﻿HttpClient client = new()
 {
-	BaseAddress = new Uri("http://localhost"),
+	BaseAddress = new Uri("http://localhost:8848"),
 };
 HttpResponseMessage msg = await client.GetAsync("ts.mp4");
 if (msg.IsSuccessStatusCode)
 {
-	Console.WriteLine(msg.Content.Headers);
+	Console.WriteLine(msg.Headers);
+	FileStream file = File.Open("./ts.mp4", FileMode.OpenOrCreate);
+	using (file)
+	{
+		Stream httpStream = await msg.Content.ReadAsStreamAsync();
+		await httpStream.CopyToAsync(file);
+		await file.FlushAsync();
+		Console.WriteLine("完成");
+	}
 }
+
+Console.ReadLine();
