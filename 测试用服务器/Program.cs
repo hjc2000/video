0f88ea07b61f255a9bbab5d33a0fd8c6bcd39a55
hjc2000 <hjc.666@foxmail.com>
2023-05-22 23:31:43 +0800

    添加注释

--- 测试用服务器/Program.cs
+++ 测试用服务器/Program.cs
@@ -58,7 +58,9 @@ app.MapGet("/ts.mp4", async (HttpContext context) =>
 		context.Response.Headers.Add("Transfer-Encoding", "chunked");
 		context.Response.Headers.ContentType = "video/mp4";
 		FileStream fileStream = File.OpenRead(_webRootPath + "/ts.mp4");
+		// 在这里使用 ChunkEncoder 从文件流中读取数据，写到 http 响应流中
 		await ChunkEncoder.WriteContentAsync(fileStream, context.Response.Body);
+		// 所有数据都写完了需要调用 WriteTrailerAsync 写入结束标志从而结束本次传输
 		await ChunkEncoder.WriteTrailerAsync(context.Response.Body);
 	}
 	catch (Exception ex)
