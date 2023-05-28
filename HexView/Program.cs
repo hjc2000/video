3abafcf52322f8040e619f563fe4f63b0be74f30
hjc2000 <hjc.666@foxmail.com>
2023-05-29 00:00:19 +0800

    完成HexView

---
+++ HexView/Program.cs
@@ -0,0 +1,31 @@
+﻿using FileStream fs = File.OpenRead("./ts0.ts");
+using BinaryReader binaryReader = new(fs);
+using FileStream outputTxt = File.Open("./hex.txt", FileMode.Create);
+using StreamWriter streamWriter = new(outputTxt);
+
+int startPos = 188;
+int endPos = 188 * 2;
+
+fs.Seek(startPos, SeekOrigin.Begin);
+
+for (int i = 0; i < endPos - startPos; i++)
+{
+	try
+	{
+		byte[] bs = binaryReader.ReadBytes(20);
+		foreach (byte b in bs)
+		{
+			streamWriter.Write($"{b:x}\t");
+		}
+
+		streamWriter.WriteLine();
+	}
+	catch
+	{
+		break;
+	}
+}
+
+streamWriter.Flush();
+Console.WriteLine("完成");
+Console.ReadLine();
