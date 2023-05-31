ad75175a6ead9271ebb571192707237ec3d62e7e
hjc2000 <hjc.666@foxmail.com>
2023-05-31 21:59:21 +0800

    使用 TaskCompletionSource 取代 TaskLib

--- BlazorApp1/Pages/Index.razor.cs
+++ BlazorApp1/Pages/Index.razor.cs
@@ -1,40 +1,30 @@
 ﻿using JSLib;
 using Microsoft.AspNetCore.Components.Forms;
 using Microsoft.JSInterop;
-using TaskLib;
 
 namespace BlazorApp1.Pages;
 public partial class Index
 {
 	#region 生命周期
-	public Index()
-	{
-		_initializer = new(async Task () =>
-		{
-			_jsModule = await JSModule.CreateAsync(JS, "./Pages/Index.razor.js");
-			_jsOp = await JSOp.CreateAsync(JS);
-			await _jsOp.AddScript("https://cdn.jsdelivr.net/npm/mpegts.js@1.7.3/dist/mpegts.min.js");
-		});
-	}
-
 	protected override async Task OnInitializedAsync()
 	{
 		await base.OnInitializedAsync();
-		_initializer.Start();
-		await _initializer.WaitAsync();
-
+		_jsModule = await JSModule.CreateAsync(JS, "./Pages/Index.razor.js");
+		_jsOp = await JSOp.CreateAsync(JS);
+		await _jsOp.AddScript("https://cdn.jsdelivr.net/npm/mpegts.js@1.7.3/dist/mpegts.min.js");
+		_initTask.SetResult();
 	}
 	#endregion
 
 	private async Task Onclick()
 	{
-		await _initializer.WaitAsync();
+		await _initTask.Task;
 		await _jsModule.InvokeVoidAsync("Log");
 	}
 
 	private async Task OnLoadFile(InputFileChangeEventArgs e)
 	{
-		await _initializer.WaitAsync();
+		await _initTask.Task;
 		Stream stream = e.File.OpenReadStream(maxAllowedSize: (long)300e6);
 		DotNetStreamReference dotNetStreamReference = new(stream);
 		_jsOp.Log(dotNetStreamReference);
@@ -42,9 +32,5 @@ public partial class Index
 
 	private JSModule _jsModule = default!;
 	private JSOp _jsOp = default!;
-
-	/// <summary>
-	/// 安装本类的依赖的任务
-	/// </summary>
-	private readonly Initializer _initializer;
+	private readonly TaskCompletionSource _initTask = new();
 }
--- 流式加载图片视频/Pages/Index.razor.cs
+++ 流式加载图片视频/Pages/Index.razor.cs
@@ -1,7 +1,6 @@
 ﻿using JSLib;
 using Microsoft.AspNetCore.Components;
 using Microsoft.JSInterop;
-using TaskLib;
 
 namespace 流式加载图片视频.Pages;
 public partial class Index
@@ -10,34 +9,30 @@ public partial class Index
 	public Index()
 	{
 		_dotnetHelper = DotNetObjectReference.Create(this);
-		_initializer = new Initializer(async () =>
-		{
-			_jsModule = await JSModule.CreateAsync(JS, "./Pages/Index.razor.js");
-			_jsOp = await JSOp.CreateAsync(JS);
-			await _jsOp.AddScript("https://cdn.jsdelivr.net/npm/mpegts.js@1.7.3/dist/mpegts.min.js");
-			await _jsOp.AddScript("./mux.js");
-		});
 	}
 
 	protected override async Task OnInitializedAsync()
 	{
 		await base.OnInitializedAsync();
-		_initializer.Start();
-		await _initializer.WaitAsync();
+		_jsModule = await JSModule.CreateAsync(JS, "./Pages/Index.razor.js");
+		_jsOp = await JSOp.CreateAsync(JS);
+		await _jsOp.AddScript("https://cdn.jsdelivr.net/npm/mpegts.js@1.7.3/dist/mpegts.min.js");
+		await _jsOp.AddScript("./mux.js");
+		_initTask.SetResult();
 	}
 	#endregion
 
 	private async Task Onclick()
 	{
-		await _initializer.WaitAsync();
+		await _initTask.Task;
 		await _jsModule.InvokeVoidAsync("LoadTS", _videoElement, _dotnetHelper);
 	}
 
 	#region 工具
 	private JSModule _jsModule = default!;
 	private JSOp _jsOp = default!;
-	private readonly Initializer _initializer;
-	DotNetObjectReference<Index> _dotnetHelper;
+	private readonly DotNetObjectReference<Index> _dotnetHelper;
+	private readonly TaskCompletionSource _initTask = new();
 	#endregion
 
 	private ElementReference _videoElement = default!;
