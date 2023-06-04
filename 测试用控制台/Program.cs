66ae38d3f376326c28ae624c6830604399e2722a
hjc2000 <hjc.666@foxmail.com>
2023-06-04 11:53:47 +0800

    使用DllImport的方式执行了gcc编译出的使用了ffmpeg库的dll

--- 测试用控制台/Program.cs
+++ 测试用控制台/Program.cs
@@ -1,11 +1,22 @@
-﻿using StreamLib;
+﻿using System.Runtime.InteropServices;
 
-string url = @"D:\my_files\workspace\wwwroot\wwwroot\ts0.ts";
-using FileStream fileStream = File.Open(url, FileMode.Open);
-byte[] buff = new byte[188];
-int haveRead = await fileStream.ReadAsync(buff);
-if (haveRead == 188)
+//string url = @"D:\my_files\workspace\wwwroot\wwwroot\ts0.ts";
+//using FileStream fileStream = File.Open(url, FileMode.Open);
+//byte[] buff = new byte[188];
+//int haveRead = await fileStream.ReadAsync(buff);
+//if (haveRead == 188)
+//{
+//	TSPacket tsPacket = new(buff);
+//	Console.WriteLine(tsPacket);
+//}
+
+Test.TestDic();
+
+public class Test
 {
-	TSPacket tsPacket = new(buff);
-	Console.WriteLine(tsPacket);
+	[DllImport(@"C:\msys64\home\huang\ffmpeg_learn\test.dll",
+		CallingConvention = CallingConvention.Cdecl,
+		CharSet = CharSet.Unicode, 
+		EntryPoint = "test_dic")]
+	public static extern void TestDic();
 }
