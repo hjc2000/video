f0a81b26f50c7ef5af4ece0c3dc74d591a1c2c4d
hjc2000 <hjc.666@foxmail.com>
2023-06-04 13:55:02 +0800

    完成 P/Invoke 测试，恢复原状

--- 测试用控制台/Program.cs
+++ 测试用控制台/Program.cs
@@ -1,22 +1,11 @@
-﻿using System.Runtime.InteropServices;
+﻿using StreamLib;
 
-//string url = @"D:\my_files\workspace\wwwroot\wwwroot\ts0.ts";
-//using FileStream fileStream = File.Open(url, FileMode.Open);
-//byte[] buff = new byte[188];
-//int haveRead = await fileStream.ReadAsync(buff);
-//if (haveRead == 188)
-//{
-//	TSPacket tsPacket = new(buff);
-//	Console.WriteLine(tsPacket);
-//}
-
-Test.TestDic();
-
-public class Test
+string url = @"D:\my_files\workspace\wwwroot\wwwroot\ts0.ts";
+using FileStream fileStream = File.Open(url, FileMode.Open);
+byte[] buff = new byte[188];
+int haveRead = await fileStream.ReadAsync(buff);
+if (haveRead == 188)
 {
-	[DllImport(@"C:\msys64\home\huang\ffmpeg_learn\test.dll",
-		CallingConvention = CallingConvention.Cdecl,
-		CharSet = CharSet.Unicode,
-		EntryPoint = "test_dic")]
-	public static extern void TestDic();
+	TSPacket tsPacket = new(buff);
+	Console.WriteLine(tsPacket);
 }
