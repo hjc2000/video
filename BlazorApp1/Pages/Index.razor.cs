7c35ee5a5caade9bfb7c490a9be20323032a4d05
hjc2000 <hjc.666@foxmail.com>
2023-06-12 16:32:54 +0800

    让InputFileElementWrapper自己创建一个input标签

--- BlazorApp1/Pages/Index.razor.cs
+++ BlazorApp1/Pages/Index.razor.cs
@@ -1,6 +1,4 @@
 ﻿using JSLib;
-using Microsoft.AspNetCore.Components;
-using Microsoft.JSInterop;
 
 namespace BlazorApp1.Pages;
 
@@ -18,23 +16,23 @@ public partial class Index
 	private async Task OnClick()
 	{
 		await _initTask.Task;
-		await _jsOp.TriggerClickEvent(_inputElement);
+		_inputFileElementWrapper = new(JS);
+		await _inputFileElementWrapper.Click();
 	}
 
-	private async Task OnFileLoad()
-	{
-		await _initTask.Task;
-		_inputFileElementWrapper = new(JS, _inputElement);
-		await using IJSObjectReference jsFileStream = await _inputFileElementWrapper.GetFileAs_JS_Stream(0);
-		_jsOp.Log(jsFileStream);
-		JSStreamReader jsStreamReader = new(JS, jsFileStream);
-		byte[] buffer = await jsStreamReader.ReadAsync();
-		Console.WriteLine(buffer.Length);
-		await _inputFileElementWrapper.Remove();
-	}
+	//private async Task OnFileLoad()
+	//{
+	//	await _initTask.Task;
+	//	_inputFileElementWrapper = new(JS, _inputElement);
+	//	await using IJSObjectReference jsFileStream = await _inputFileElementWrapper.GetFileAs_JS_Stream(0);
+	//	_jsOp.Log(jsFileStream);
+	//	JSStreamReader jsStreamReader = new(JS, jsFileStream);
+	//	byte[] buffer = await jsStreamReader.ReadAsync();
+	//	Console.WriteLine(buffer.Length);
+	//	await _inputFileElementWrapper.Remove();
+	//}
 
 	private TaskCompletionSource _initTask = new();
-	private ElementReference _inputElement = default!;
 	private InputFileElementWrapper _inputFileElementWrapper = default!;
 	private JSModule _jsModule = default!;
 	private JSOp _jsOp = default!;
