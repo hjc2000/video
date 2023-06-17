e7702d7f63454ebe085ca4c40dadde609afe6602
hjc2000 <huangjincanqqmail@qq.com>
2023-06-17 12:49:35 +0800

    添加 C++ 项目

---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+class Person
+{
+public:
+	Person()
+	{
+		cout << "Person 构造函数" << endl;
+	}
+	~Person()
+	{
+		cout << "析构函数" << endl;
+	}
+	Person(const Person& person)
+	{
+		cout << "拷贝构造函数" << endl;
+	}
+};
+
+int main(void)
+{
+	// test_dic();
+	Person p;
+	Person p1{ p };
+	return 0;
+}
