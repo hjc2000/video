e1f9ae5f0560fc051653fb0d764c1c538d2f8a3b
hjc2000 <huangjincanqqmail@qq.com>
2023-07-04 23:42:00 +0800

    整理代码

--- ffmpeg/encode.cpp
+++ ffmpeg/encode.cpp
@@ -16,23 +16,23 @@ static void encode(FFmpeg::AVCodecContext enc_ctx, FFmpeg::AVFrame frame, fstrea
 /// </summary>
 void encode_main()
 {
-	int i, ret, x, y;
-	fstream fs{ "output.mp4", ios_base::out | ios_base::in | ios_base::trunc | ios_base::binary };
+	fstream outputFS{ "output.mp4", ios_base::out | ios_base::in | ios_base::trunc | ios_base::binary };
 	uint8_t endcode[] = { 0, 0, 1, 0xb7 };
 	const char *outputFileName = "output.mp4";
 
 	/* find the mpeg1video encoder */
-	FFmpeg::AVCodec codec = FFmpeg::AVCodec::find_encoder_by_name("mpeg1video");
-	FFmpeg::AVCodecContext codecCtx = FFmpeg::AVCodecContext::create(codec);
+	FFmpeg::AVCodec encoder = FFmpeg::AVCodec::find_encoder_by_name("mpeg1video");
+	FFmpeg::AVCodecContext encoderCtx = FFmpeg::AVCodecContext::create(encoder);
 
+	#pragma region 手动设置编码器上下文的参数
 	/* put sample parameters */
-	codecCtx()->bit_rate = 400000;
+	encoderCtx()->bit_rate = 400000;
 	/* resolution must be a multiple of two */
-	codecCtx()->width = 352;
-	codecCtx()->height = 288;
+	encoderCtx()->width = 352;
+	encoderCtx()->height = 288;
 	/* frames per second */
-	codecCtx()->time_base = AVRational{ 1, 25 };
-	codecCtx()->framerate = AVRational{ 25, 1 };
+	encoderCtx()->time_base = AVRational{ 1, 25 };
+	encoderCtx()->framerate = AVRational{ 25, 1 };
 
 	/* emit one intra frame every ten frames
 	* check frame pict_type before passing frame
@@ -40,24 +40,27 @@ void encode_main()
 	* then gop_size is ignored and the output of encoder
 	* will always be I frame irrespective to gop_size
 	*/
-	codecCtx()->gop_size = 10;
-	codecCtx()->max_b_frames = 1;
-	codecCtx()->pix_fmt = AV_PIX_FMT_YUV420P;
+	encoderCtx()->gop_size = 10;
+	encoderCtx()->max_b_frames = 1;
+	encoderCtx()->pix_fmt = AV_PIX_FMT_YUV420P;
+	#pragma endregion
 
-	if (codec()->id == AV_CODEC_ID_H264)
-		av_opt_set(codecCtx()->priv_data, "preset", "slow", 0);
+	if (encoder()->id == AV_CODEC_ID_H264)
+	{
+		av_opt_set(encoderCtx()->priv_data, "preset", "slow", 0);
+	}
 
 	/* open it */
-	codecCtx.open();
+	encoderCtx.open();
 
 	FFmpeg::AVFrame frame;
 	frame = FFmpeg::AVFrame::create();
-	frame()->format = codecCtx()->pix_fmt;
-	frame()->width = codecCtx()->width;
-	frame()->height = codecCtx()->height;
+	frame()->format = encoderCtx()->pix_fmt;
+	frame()->width = encoderCtx()->width;
+	frame()->height = encoderCtx()->height;
 	frame.av_frame_get_buffer(0);
 	/* encode 1 second of video */
-	for (i = 0; i < 25; i++)
+	for (int i = 0; i < 25; i++)
 	{
 		/* Make sure the frame data is writable.
 		On the first round, the frame is fresh from av_frame_get_buffer()
@@ -77,18 +80,18 @@ void encode_main()
 		frame.
 		*/
 		/* Y */
-		for (y = 0; y < codecCtx()->height; y++)
+		for (int y = 0; y < encoderCtx()->height; y++)
 		{
-			for (x = 0; x < codecCtx()->width; x++)
+			for (int x = 0; x < encoderCtx()->width; x++)
 			{
 				frame()->data[0][y * frame()->linesize[0] + x] = x + y + i * 3;
 			}
 		}
 
 		/* Cb and Cr */
-		for (y = 0; y < codecCtx()->height / 2; y++)
+		for (int y = 0; y < encoderCtx()->height / 2; y++)
 		{
-			for (x = 0; x < codecCtx()->width / 2; x++)
+			for (int x = 0; x < encoderCtx()->width / 2; x++)
 			{
 				frame()->data[1][y * frame()->linesize[1] + x] = 128 + y + i * 2;
 				frame()->data[2][y * frame()->linesize[2] + x] = 64 + x + i * 5;
@@ -98,11 +101,11 @@ void encode_main()
 		frame()->pts = i;
 
 		/* encode the image */
-		encode(codecCtx, frame, fs);
+		encode(encoderCtx, frame, outputFS);
 	}
 
 	/* flush the encoder */
-	encode(codecCtx, nullptr, fs);
+	encode(encoderCtx, nullptr, outputFS);
 
 	/* Add sequence end code to have a real MPEG file.
 	It makes only sense because this tiny examples writes packets
@@ -110,9 +113,11 @@ void encode_main()
 	codecs. To create a valid file, you usually need to write packets
 	into a proper file format or protocol; see mux.c.
 	*/
-	if (codec()->id == AV_CODEC_ID_MPEG1VIDEO || codec()->id == AV_CODEC_ID_MPEG2VIDEO)
-		fs.write((char *)endcode, sizeof(endcode));
+	if (encoder()->id == AV_CODEC_ID_MPEG1VIDEO || encoder()->id == AV_CODEC_ID_MPEG2VIDEO)
+	{
+		outputFS.write((char *)endcode, sizeof(endcode));
+	}
 
-	fs.flush();
-	fs.close();
+	outputFS.flush();
+	outputFS.close();
 }
--- ffmpeg/include/AVCodecContext.cpp
+++ ffmpeg/include/AVCodecContext.cpp
@@ -1,6 +1,47 @@
 #include "AVCodecContext.h"
 #include<AVDictionary.h>
 
+void FFmpeg::AVCodecContext::Dispose()
+{
+	if (should_dispose())
+	{
+		cout << "AVCodecContext 释放" << endl;
+		avcodec_free_context(&_pWrapedObj);
+	}
+}
+
+FFmpeg::AVCodecContext FFmpeg::AVCodecContext::create(FFmpeg::AVCodec codec)
+{
+	FFmpeg::AVCodecContext ctx;
+	ctx._codec = codec;
+	ctx._pWrapedObj = ::avcodec_alloc_context3(codec);
+	if (!ctx._pWrapedObj)
+	{
+		throw Exception("FFmpeg::AVCodecContext create(FFmpeg::AVCodec codec) 失败");
+	}
+
+	return ctx;
+}
+
+FFmpeg::AVCodecContext FFmpeg::AVCodecContext::create(FFmpeg::AVCodec codec, AVCodecParameters *param, bool autoOpen)
+{
+	FFmpeg::AVCodecContext ctx = create(codec);
+	int ret = ::avcodec_parameters_to_context(ctx._pWrapedObj, param);
+	if (ret < 0)
+	{
+		throw Exception("AVCodecContext::create", ret);
+	}
+	else
+	{
+		if (autoOpen)
+		{
+			ctx.open();
+		}
+
+		return ctx;
+	}
+}
+
 void FFmpeg::AVCodecContext::open(FFmpeg::AVDictionary dic)
 {
 	int ret = ::avcodec_open2(_pWrapedObj, _codec, dic);
