73836f81a404a9191173f6bb47d1f29bc8ba6810
hjc2000 <huangjincanqqmail@qq.com>
2024-03-22 10:32:17 +0800

    InfinitePacketPipe

--- cpp/ffmpeg-wrapper/src/pipe/function-pipe/InfinitePacketPipe.cpp
+++ cpp/ffmpeg-wrapper/src/pipe/function-pipe/InfinitePacketPipe.cpp
@@ -3,6 +3,46 @@
 using namespace video;
 using namespace std;
 
+void InfinitePacketPipe::UpdateLastPacketDuration(int64_t value)
+{
+	if (value > _last_packet_duration)
+	{
+		_last_packet_duration = value;
+	}
+}
+
+void InfinitePacketPipe::UpdateLastPts(int64_t value)
+{
+	if (value > _last_pts)
+	{
+		_last_pts = value;
+	}
+}
+
+void InfinitePacketPipe::UpdateLastDts(int64_t value)
+{
+	if (value > _last_dts)
+	{
+		_last_dts = value;
+	}
+}
+
+void InfinitePacketPipe::CorrectStartTimeStamp(AVPacketWrapper &packet)
+{
+	if (_start_pts_dts_not_set)
+	{
+		_start_pts_dts_not_set = false;
+		_start_pts = packet.pts();
+		_start_dts = packet.dts();
+
+		// 开头处对齐取最小值，因为不能出现时间戳回溯。
+		_correct_offset = std::min(_start_pts, _start_dts);
+	}
+
+	packet.set_pts(packet.pts() - _correct_offset);
+	packet.set_dts(packet.dts() - _correct_offset);
+}
+
 void InfinitePacketPipe::SendPacket(AVPacketWrapper *packet)
 {
 	if (!packet)
@@ -29,4 +69,9 @@ void InfinitePacketPipe::SendPacket(AVPacketWrapper *packet)
 	packet->set_pts(packet->pts() + _offset);
 	packet->set_dts(packet->dts() + _offset);
 	SendPacketToEachConsumer(packet);
+}
+
+void InfinitePacketPipe::FlushConsumer()
+{
+	SendPacketToEachConsumer(nullptr);
 }
--- cpp/ffmpeg-wrapper/src/pipe/function-pipe/InfinitePacketPipe.h
+++ cpp/ffmpeg-wrapper/src/pipe/function-pipe/InfinitePacketPipe.h
@@ -17,29 +17,9 @@ namespace video
 		int64_t _offset = 0;
 		int64_t _last_packet_duration = 0;
 
-		void UpdateLastPacketDuration(int64_t value)
-		{
-			if (value > _last_packet_duration)
-			{
-				_last_packet_duration = value;
-			}
-		}
-
-		void UpdateLastPts(int64_t value)
-		{
-			if (value > _last_pts)
-			{
-				_last_pts = value;
-			}
-		}
-
-		void UpdateLastDts(int64_t value)
-		{
-			if (value > _last_dts)
-			{
-				_last_dts = value;
-			}
-		}
+		void UpdateLastPacketDuration(int64_t value);
+		void UpdateLastPts(int64_t value);
+		void UpdateLastDts(int64_t value);
 
 		#pragma region 校准起始时间戳
 	private:
@@ -48,40 +28,28 @@ namespace video
 		int64_t _start_dts = 0;
 		int64_t _correct_offset = 0;
 
-		void CorrectStartTimeStamp(AVPacketWrapper &packet)
-		{
-			if (_start_pts_dts_not_set)
-			{
-				_start_pts_dts_not_set = false;
-				_start_pts = packet.pts();
-				_start_dts = packet.dts();
-
-				// 开头处对齐取最小值，因为不能出现时间戳回溯。
-				_correct_offset = std::min(_start_pts, _start_dts);
-			}
-
-			packet.set_pts(packet.pts() - _correct_offset);
-			packet.set_dts(packet.dts() - _correct_offset);
-		}
+		/// <summary>
+		///		如果有一个文件的第一个包的时间戳就是很大的值，则取 pts 和 dts
+		///		的最小值，让 pts 和 dts 减去这个最小值。这样可让时间戳起始点最接近 0.
+		/// </summary>
+		/// <param name="packet"></param>
+		void CorrectStartTimeStamp(AVPacketWrapper &packet);
 		#pragma endregion
 
 	public:
 		/// <summary>
-		///		送入包。会在调整包的 pts 和 dts 后送给消费者。如果送入空指针，会直接返回，不会将空指针送给消费者，
-		///		所以本函数无法冲洗消费者。冲洗消费者需要调用 FlushConsumer 函数。
+		///		送入包。会在调整包的 pts 和 dts 后送给消费者。
 		/// 
-		///		* 送进来的包的时间基必须是 1/90000，否则会抛出异常。
+		///		如果送入空指针，不会将空指针送给消费者，空指针在这里的作用是表示当前文件的包读完了，
+		///		将开始下一个文件。下一个包会发生时间戳的回溯。这时候需要更新内部记录的偏移量，
+		///		等新文件的包开始后，需要将时间戳加上偏移量。所以本函数无法冲洗消费者。
+		///		冲洗消费者需要调用 FlushConsumer 函数。
+		/// 
+		///		送进来的包的时间基必须是 1/90000，否则会抛出异常。
 		/// </summary>
 		/// <param name="packet"></param>
 		void SendPacket(AVPacketWrapper *packet) override;
 
-		/// <summary>
-		///		向 send_packet 送入空指针会直接返回，不会向消费者送入空指针从而冲洗消费者。
-		///		只有调用本函数才会向消费者送入空指针，从而冲洗消费者。
-		/// </summary>
-		void FlushConsumer()
-		{
-			SendPacketToEachConsumer(nullptr);
-		}
+		void FlushConsumer();
 	};
 }
