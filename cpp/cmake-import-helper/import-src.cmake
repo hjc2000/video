c072768e315e472170f64da119f3bcb6d67ee35b
hjc2000 <huangjincanqqmail@qq.com>
2024-03-29 22:50:22 +0800

    target_import_src

--- cpp/cmake-import-helper/import-src.cmake
+++ cpp/cmake-import-helper/import-src.cmake
@@ -1,3 +1,4 @@
+# 导入项目文件夹中的 src 文件夹内的源文件和头文件
 function(target_import_src target_name)
     target_add_source_recurse(${target_name} ${CMAKE_CURRENT_SOURCE_DIR}/src/)
     add_and_install_headers_recurse(${target_name} ${CMAKE_CURRENT_SOURCE_DIR}/src/)
