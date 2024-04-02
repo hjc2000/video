882a96a928e50a5f9225b91fb4e715391a504688
hjc <96368006+hjc2000@users.noreply.github.com>
2024-04-02 10:47:32 +0800

    BlockingCircleBufferMemoryStream

--- cpp/jccpp/src/stream/BlockingCircleBufferMemoryStream.h
+++ cpp/jccpp/src/stream/BlockingCircleBufferMemoryStream.h
@@ -32,13 +32,14 @@ namespace jc
 		///		流中的数据被消费了，现在处于不是满的状态
 		/// </summary>
 		std::condition_variable _buffer_consumed;
+
 		/// <summary>
 		///		流中有数据可用。
 		/// </summary>
 		std::condition_variable _buffer_avaliable;
 
 	public:
-		// 通过 Stream 继承
+		#pragma region 通过 Stream 继承
 		bool CanRead() override;
 		bool CanWrite() override;
 		bool CanSeek() override;
@@ -50,12 +51,14 @@ namespace jc
 
 		/// <summary>
 		///		结束流。
-		///		* 结束后，写入的数据会被丢弃。Read 方法在读取完缓冲区的数据后，将永远返回 0.
-		///		* 结束后，会取消所有阻塞，且不会再阻塞。
+		///		- 结束后，写入的数据会被丢弃。Read 方法在读取完缓冲区的数据后，将永远返回 0.
+		///		- 结束后，会取消所有阻塞，且不会再阻塞。
 		/// </summary>
 		void Close() override;
 
 		int64_t Position() override;
 		void SetPosition(int64_t value) override;
+		#pragma endregion
+
 	};
 }
