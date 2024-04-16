33446f6623d2171bebf2f052ff80c51a50bef015
hjc <96368006+hjc2000@users.noreply.github.com>
2024-04-16 16:58:25 +0800

    1

--- 测试用控制台/测试用控制台.cs
+++ 测试用控制台/测试用控制台.cs
@@ -1,17 +1,21 @@
 ﻿using PInvokeLib;
 using System.Runtime.InteropServices;
 
-FileStream video_file = File.Open(
+using FileStream video_file = File.Open(
 	"D:/repos/video/cpp/test/资源/idol.mp4",
 	FileMode.Open,
 	FileAccess.Read,
 	FileShare.Read
 );
-DotNetStreamHelper dotnet_stream_helper = new(video_file);
+using DotNetStreamHelper dotnet_stream_helper = new(video_file);
 Test.ReencodeDotNetVideoStream(dotnet_stream_helper.CppObjRawPtr);
 
 internal partial class Test
 {
+	/// <summary>
+	///		需要改成能够重复打开文件。否则ffmpeg读取文件完了会Close
+	/// </summary>
+	/// <param name="dotnet_stream"></param>
 	[LibraryImport("libffmpeg-wrapper", EntryPoint = "ReencodeDotNetVideoStream")]
 	public static unsafe partial void ReencodeDotNetVideoStream(nuint dotnet_stream);
 }
