51d8613221225b0070523adf7722298ebd7f3793
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2024-04-18 18:16:41 +0800

    1

--- HexView/Program.cs
+++ HexView/Program.cs
@@ -38,7 +38,7 @@ static async Task HandleAsync(string input_file_path, string? output_file_path,
 		{
 			byte b = binary_reader.ReadByte();
 			port.Write($"{b:x2}\t");
-			if ((i + 1) % 20 == 0)
+			if ((i + 1) % 10 == 0)
 			{
 				port.WriteLine();
 			}
