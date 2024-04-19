0d589c73e11f1abf3aa2b058b4b5a73d78a80992
hjc <96368006+hjc2000@users.noreply.github.com>
2024-04-19 11:31:37 +0800

    AVMixer

--- cpp/ffmpeg-wrapper/include/ffmpeg-wrapper/mux/AVMixer.cpp
+++ cpp/ffmpeg-wrapper/include/ffmpeg-wrapper/mux/AVMixer.cpp
@@ -5,10 +5,54 @@ using namespace video;
 
 void video::AVMixer::CreateNewVideoStream()
 {
-	AVStreamWrapper video_stream = _input_video_format->FindBestStream(AVMediaType::AVMEDIA_TYPE_VIDEO);
-	AVStreamWrapper new_video_stream = _out_format->CreateNewStream();
-	new_video_stream.SetCodecParams(video_stream.CodecParams());
-	new_video_stream.SetIndex(0);
+	AVStreamWrapper stream = _input_video_format->FindBestStream(AVMediaType::AVMEDIA_TYPE_VIDEO);
+	AVStreamWrapper new_stream = _out_format->CreateNewStream();
+	new_stream.SetCodecParams(stream.CodecParams());
+	new_stream.SetIndex(0);
+}
+
+void video::AVMixer::CreateNewAudioStream()
+{
+	AVStreamWrapper stream = _input_video_format->FindBestStream(AVMediaType::AVMEDIA_TYPE_AUDIO);
+	AVStreamWrapper new_stream = _out_format->CreateNewStream();
+	new_stream.SetCodecParams(stream.CodecParams());
+	new_stream.SetIndex(1);
+}
+
+bool video::AVMixer::ReadVideoPacketOnce()
+{
+	int result = _input_video_format->ReadPacket(_temp_packet);
+	switch (result)
+	{
+	case 0:
+		{
+			_temp_packet.SetStreamIndex(0);
+			_infinite_packet_pipe->SendPacket(&_temp_packet);
+			return true;
+		}
+	default:
+		{
+			return false;
+		}
+	}
+}
+
+bool video::AVMixer::ReadAudioPacketOnce()
+{
+	int result = _input_video_format->ReadPacket(_temp_packet);
+	switch (result)
+	{
+	case 0:
+		{
+			_temp_packet.SetStreamIndex(1);
+			_infinite_packet_pipe->SendPacket(&_temp_packet);
+			return true;
+		}
+	default:
+		{
+			return false;
+		}
+	}
 }
 
 video::AVMixer::AVMixer(
@@ -20,10 +64,20 @@ video::AVMixer::AVMixer(
 	_input_video_format = input_video_format;
 	_input_audio_format = input_audio_format;
 	_out_format = out_format;
+	_infinite_packet_pipe->PacketConsumerList().Add(_out_format);
+
 	CreateNewVideoStream();
+	CreateNewAudioStream();
 }
 
 void video::AVMixer::Pump(shared_ptr<CancellationToken> cancel_pump)
 {
-
+	while (!cancel_pump->IsCancellationRequested())
+	{
+		if (!ReadVideoPacketOnce() && !ReadAudioPacketOnce())
+		{
+			_infinite_packet_pipe->FlushConsumer();
+			return;
+		}
+	}
 }
--- cpp/ffmpeg-wrapper/include/ffmpeg-wrapper/mux/AVMixer.h
+++ cpp/ffmpeg-wrapper/include/ffmpeg-wrapper/mux/AVMixer.h
@@ -1,6 +1,8 @@
 #pragma once
+#include<ffmpeg-wrapper/ErrorCode.h>
 #include<ffmpeg-wrapper/input-format/InputFormat.h>
 #include<ffmpeg-wrapper/output-format/OutputFormat.h>
+#include<ffmpeg-wrapper/pipe/InfinitePacketPipe.h>
 #include<ffmpeg-wrapper/pipe/interface/IPump.h>
 
 namespace video
@@ -16,19 +18,13 @@ namespace video
 		shared_ptr<InputFormat> _input_video_format;
 		shared_ptr<InputFormat> _input_audio_format;
 		shared_ptr<OutputFormat> _out_format;
-
-		/* 音视频的起始时间戳可能不对齐，需要都调整到 0。在收到第一个包的时候就要将时间戳记录下来，
-		* 之后的每个包都要减去记录的时间戳。
-		*
-		* 包的时间戳有 2 个，即 pts 和 dts。要选择这两个中的最小值作为偏移量，如果选择最大值作为
-		* 偏移量，会导致其中一个时间戳为负数。
-		*/
-		int64_t _video_time_stamp_offset = 0;
-		int64_t _audio_time_stamp_offset = 0;
-		bool _is_first_video_packet = true;
-		bool _is_first_audio_packet = true;
+		shared_ptr<InfinitePacketPipe> _infinite_packet_pipe{ new InfinitePacketPipe{} };
+		AVPacketWrapper _temp_packet;
 
 		void CreateNewVideoStream();
+		void CreateNewAudioStream();
+		bool ReadVideoPacketOnce();
+		bool ReadAudioPacketOnce();
 
 	public:
 		AVMixer(
