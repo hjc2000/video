ac252068c48840864f0a21141d30698449edcaa1
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2024-05-16 00:37:25 +0800

    jccpp::ModbusCrc16 crc16;

---
+++ cpp/crc/main.cpp
@@ -0,0 +1,22 @@
+﻿#include<bit>
+#include<iostream>
+#include<jccpp/Bit.h>
+#include<jccpp/math/ModbusCrc16.h>
+#include<stdint.h>
+
+int main(void)
+{
+	jccpp::ModbusCrc16 crc16;
+	crc16.Add(0x01);
+	crc16.Add(0x10);
+	crc16.Add(0x40);
+	crc16.Add(0x00);
+	crc16.Add(0x00);
+	crc16.Add(0x02);
+	crc16.Add(0x04);
+	crc16.Add(0x00);
+	crc16.Add(0x00);
+	crc16.Add(0x00);
+	crc16.Add(0x01);
+	std::cout << crc16.RegisterValue() << std::endl;
+}
