fd730d65fbb37256cc66e746d0188318f6c2ba64
hjc2000 <hjc.666@foxmail.com>
2024-05-16 15:58:41 +0800

    成功让伺服停止

---
+++ Modbus/Program.cs
@@ -0,0 +1,12 @@
+﻿using System.IO.Ports;
+
+SerialPort port = new("COM5")
+{
+	BaudRate = 115200,
+	Parity = Parity.Even,
+	StopBits = StopBits.One
+};
+
+port.Open();
+byte[] data = [0x01, 0x05, 0x02, 0x08, 0x00, 0x00, 0x0C, 0x40, 0x30, 0x14];
+port.Write(data, 0, data.Length);
