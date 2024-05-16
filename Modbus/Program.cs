5cad2ca0b77d894b10e30c3544b124967665fc32
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2024-05-16 22:37:58 +0800

    SerialPort serial = new("COM5")

--- Modbus/Program.cs
+++ Modbus/Program.cs
@@ -1,13 +1,13 @@
 ﻿using libsdv3.Modbus;
 using System.IO.Ports;
 
-SerialPort port = new("COM5")
+SerialPort serial = new("COM5")
 {
 	BaudRate = 115200,
 	Parity = Parity.Even,
 	StopBits = StopBits.One
 };
 
-port.Open();
-ModbusSdv3Device sdv3 = new(port, 1);
+serial.Open();
+ModbusSdv3Device sdv3 = new(serial, 1);
 Console.WriteLine(sdv3.EI9);
