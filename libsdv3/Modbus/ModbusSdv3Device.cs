5ceaee1107f7946bcf02f2a1c8f10934003a5400
hjc2000 <hjc.666@foxmail.com>
2024-05-17 09:48:58 +0800

    GenerateWriteSingleBitFrame变成内部函数

--- libsdv3/Modbus/ModbusSdv3Device.cs
+++ libsdv3/Modbus/ModbusSdv3Device.cs
@@ -47,36 +47,6 @@ public class ModbusSdv3Device : ISdv3Device
 		return [.. frame, crc16.RegisterLowByte, crc16.RegisterHighByte];
 	}
 
-	public byte[] GenerateWriteSingleBitFrame(byte device_addr, ushort data_addr, bool value)
-	{
-		byte[] frame = new byte[6];
-		ushort data = value ? (ushort)0Xff00 : (ushort)0;
-
-		frame[0] = device_addr;
-		frame[1] = (byte)FunctionCode.WriteSingleBit;
-		if (Bigendian)
-		{
-			// 如果数据要求大端序，并且本机是小端序
-			frame[2] = (byte)(data_addr >> 8);
-			frame[3] = (byte)data_addr;
-
-			frame[4] = (byte)(data >> 8);
-			frame[5] = (byte)data;
-		}
-		else
-		{
-			frame[2] = (byte)data_addr;
-			frame[3] = (byte)(data_addr >> 8);
-
-			frame[4] = (byte)data;
-			frame[5] = (byte)(data >> 8);
-		}
-
-		ModbusCrc16 crc16 = new();
-		crc16.Add(frame);
-		return [.. frame, crc16.RegisterLowByte, crc16.RegisterHighByte];
-	}
-
 	/// <summary>
 	///		检查 ADU。
 	///		* 不包括 PDU 部分，只检查作为头部的地址和作为尾部的 CRC16。
@@ -120,6 +90,36 @@ public class ModbusSdv3Device : ISdv3Device
 
 	public void WriteSingleBit(ushort data_addr, bool value)
 	{
+		byte[] GenerateWriteSingleBitFrame(byte device_addr, ushort data_addr, bool value)
+		{
+			byte[] frame = new byte[6];
+			ushort data = value ? (ushort)0Xff00 : (ushort)0;
+
+			frame[0] = device_addr;
+			frame[1] = (byte)FunctionCode.WriteSingleBit;
+			if (Bigendian)
+			{
+				// 如果数据要求大端序，并且本机是小端序
+				frame[2] = (byte)(data_addr >> 8);
+				frame[3] = (byte)data_addr;
+
+				frame[4] = (byte)(data >> 8);
+				frame[5] = (byte)data;
+			}
+			else
+			{
+				frame[2] = (byte)data_addr;
+				frame[3] = (byte)(data_addr >> 8);
+
+				frame[4] = (byte)data;
+				frame[5] = (byte)(data >> 8);
+			}
+
+			ModbusCrc16 crc16 = new();
+			crc16.Add(frame);
+			return [.. frame, crc16.RegisterLowByte, crc16.RegisterHighByte];
+		}
+
 		_serial.Write(GenerateWriteSingleBitFrame(_device_addr, data_addr, value));
 		byte[] read_buffer = _serial.ReadExactly(8);
 		CheckADU(read_buffer);
