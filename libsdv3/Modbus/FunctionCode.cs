19e5e0bce70367b7e1d96691003a149265dfa58f
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2024-05-19 13:16:28 +0800

    FunctionCode

--- libsdv3/Modbus/FunctionCode.cs
+++ libsdv3/Modbus/FunctionCode.cs
@@ -2,7 +2,33 @@
 
 public enum FunctionCode
 {
-	ReadBits = 0x01,
+	/// <summary>
+	///		读取各种数据
+	/// </summary>
 	ReadDatas = 0x03,
+
+	/// <summary>
+	///		写入各种数据
+	/// </summary>
+	WriteDatas = 0x10,
+
+	/// <summary>
+	///		读取多个位
+	/// </summary>
+	ReadBits = 0x01,
+
+	/// <summary>
+	///		写入单个位
+	/// </summary>
 	WriteSingleBit = 0x05,
+
+	/// <summary>
+	///		写入多个位
+	/// </summary>
+	WriteBits = 0x0f,
+
+	/// <summary>
+	///		诊断
+	/// </summary>
+	Diagnosis = 0x08,
 }
