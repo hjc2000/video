93de4362f42e1d82d14f6967422a3b30125647f0
hjc2000 <hjc.666@foxmail.com>
2024-05-20 11:51:03 +0800

    ///  Interaction logic for MainWindow.xaml

--- Sdv3ControlPanel/MainWindow.xaml.cs
+++ Sdv3ControlPanel/MainWindow.xaml.cs
@@ -3,7 +3,7 @@ using System.Windows;
 
 namespace Sdv3ControlPanel;
 /// <summary>
-/// Interaction logic for MainWindow.xaml
+///		Interaction logic for MainWindow.xaml
 /// </summary>
 public partial class MainWindow : Window
 {
