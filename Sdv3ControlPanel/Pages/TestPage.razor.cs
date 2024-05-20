d54432b7855e69ab2c31e5a9b54498dc6933ad83
hjc2000 <hjc.666@foxmail.com>
2024-05-20 11:41:54 +0800

    点击按钮使能。并且有指示灯。

---
+++ Sdv3ControlPanel/Pages/TestPage.razor.cs
@@ -0,0 +1,46 @@
+﻿using JCNET.流;
+using libsdv3.Modbus;
+using System;
+using System.IO.Ports;
+using System.Threading.Tasks;
+
+namespace Sdv3ControlPanel.Pages;
+
+public partial class TestPage : IAsyncDisposable
+{
+	public TestPage()
+	{
+		_serial.Open();
+		_sdv3 = new(new SerialPortStream(_serial), 1, true);
+	}
+
+	private bool _disposed = false;
+	public async ValueTask DisposeAsync()
+	{
+		if (_disposed)
+		{
+			return;
+		}
+
+		_disposed = true;
+		GC.SuppressFinalize(this);
+
+		await ValueTask.CompletedTask;
+		_serial.Close();
+		_serial.Dispose();
+	}
+
+	private ModbusSdv3Device _sdv3;
+	private SerialPort _serial = new("COM5")
+	{
+		BaudRate = 115200,
+		Parity = Parity.Even,
+		StopBits = StopBits.One
+	};
+
+	private async Task OnEnableButtonClickAsync()
+	{
+		await Task.CompletedTask;
+		_sdv3.EI9 = !_sdv3.EI9;
+	}
+}
