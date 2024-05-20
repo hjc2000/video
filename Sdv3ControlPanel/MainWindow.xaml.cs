254d50c481d39b8db088d7bdbcee0357cb521e27
hjc2000 <hjc.666@foxmail.com>
2024-05-20 11:26:34 +0800

    Sdv3ControlPanel

---
+++ Sdv3ControlPanel/MainWindow.xaml.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System.Windows;
+
+namespace Sdv3ControlPanel;
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+	public MainWindow()
+	{
+		InitializeComponent();
+		ServiceCollection serviceCollection = new();
+		serviceCollection.AddWpfBlazorWebView();
+		Resources.Add("services", serviceCollection.BuildServiceProvider());
+	}
+}
