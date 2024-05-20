8cbd4edd23f3340e849816a02375dbf8608b36f4
hjc2000 <hjc.666@foxmail.com>
2024-05-20 10:40:16 +0800

    Console.WriteLine(sdv3.FeedbackCurrentPosition);

--- Modbus/Program.cs
+++ Modbus/Program.cs
@@ -11,10 +11,21 @@ SerialPort serial = new("COM5")
 
 serial.Open();
 ModbusSdv3Device sdv3 = new(new SerialPortStream(serial), 1, true);
+
+Console.WriteLine("pn 参数");
 Console.WriteLine(sdv3.Pn(1, 6));
 Console.WriteLine(sdv3.Pn(1, 7));
+Console.WriteLine();
+
+Console.WriteLine("报警记录：");
 Console.WriteLine($"0x{sdv3.CurrentAlarm:x2}");
 for (int i = 1; i <= 20; i++)
 {
 	Console.WriteLine($"0x{sdv3.AlarmRecords(i):x2}");
 }
+
+Console.WriteLine();
+
+Console.WriteLine("反馈当前位置：");
+Console.WriteLine(sdv3.FeedbackCurrentPosition);
+Console.WriteLine();
