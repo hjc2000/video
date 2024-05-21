9ff3778585f66195c67d460e53d193bf6c7400a6
hjc2000 <hjc.666@foxmail.com>
2024-05-21 13:42:53 +0800

    public enum AlarmCode

--- libsdv3/Code/AlarmCode.cs
+++ libsdv3/Code/AlarmCode.cs
@@ -2,9 +2,24 @@
 
 public enum AlarmCode
 {
+	/// <summary>
+	///		无错误
+	/// </summary>
 	None = 0x00,
+
+	/// <summary>
+	///		过电流 1
+	/// </summary>
 	OverCurrent1 = 0x01,
+
+	/// <summary>
+	///		过电流 2
+	/// </summary>
 	OverCurrent2 = 0x02,
+
+	/// <summary>
+	///		超速
+	/// </summary>
 	OverSpeed = 0x03,
 
 	/// <summary>
@@ -12,12 +27,39 @@ public enum AlarmCode
 	/// </summary>
 	CcontrolCircuitLowVoltage = 0x04,
 
+	/// <summary>
+	///		过电压
+	/// </summary>
 	OverVoltage = 0x05,
+
+	/// <summary>
+	///		编码器异常 1
+	/// </summary>
 	EncoderException1 = 0x06,
+
+	/// <summary>
+	///		编码器异常 2
+	/// </summary>
 	EncoderException2 = 0x07,
+
+	/// <summary>
+	///		控制电路异常
+	/// </summary>
 	CcontrolCircuitException = 0x08,
+
+	/// <summary>
+	///		储存器异常
+	/// </summary>
 	StorageDeviceException = 0x09,
+
+	/// <summary>
+	///		电机组合异常
+	/// </summary>
 	ElectricalMachineryCombinationException = 0x0B,
+
+	/// <summary>
+	///		编码器通信异常
+	/// </summary>
 	EncoderCommunicationException = 0x0D,
 
 	/// <summary>
@@ -29,8 +71,15 @@ public enum AlarmCode
 	///		过载 1
 	/// </summary>
 	OverLoad1 = 0x0F,
+
+	/// <summary>
+	///		过载 1
+	/// </summary>
 	OverLoad2 = 0x10,
 
+	/// <summary>
+	///		过电流 3
+	/// </summary>
 	OverCurrent3 = 0x12,
 
 	/// <summary>
