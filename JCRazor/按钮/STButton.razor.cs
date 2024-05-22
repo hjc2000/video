2f2050dae47305abca8a80afb570b3dbfdc6a003
hjc2000 <hjc.666@foxmail.com>
2024-05-22 10:14:23 +0800

    STButton添加Disabled属性

--- JCRazor/按钮/STButton.razor.cs
+++ JCRazor/按钮/STButton.razor.cs
@@ -5,6 +5,8 @@ public partial class STButton
 {
 	[Parameter]
 	public RenderFragment? ChildContent { get; set; }
+	[Parameter]
+	public bool Disabled { get; set; } = false;
 
 	[Parameter(CaptureUnmatchedValues = true)]
 	public Dictionary<string, object>? CaptureUnmatchedValues { get; set; }
