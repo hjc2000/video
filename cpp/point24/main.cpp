3cf8453a360fc1829092e57e3ce330dca35523f3
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2024-05-25 21:19:31 +0800

    catch (std::exception &e)

--- cpp/point24/main.cpp
+++ cpp/point24/main.cpp
@@ -6,11 +6,11 @@ int main(void)
 {
 	try
 	{
-		std::array<uint8_t, 4> nums{ 11,5,4,3 };
+		std::array<uint8_t, 4> nums { 11, 5, 4, 3 };
 		cout << Point24Solver::Solve(nums) << endl;
 		return 0;
 	}
-	catch (std::runtime_error &e)
+	catch (std::exception &e)
 	{
 		cout << e.what() << endl;
 		throw;
