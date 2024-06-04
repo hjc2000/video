c9df547416df40253db1662f4e3367f36989e95e
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2024-06-05 00:33:08 +0800

    base::Fraction a { 1, 5 };   base::Fraction b { 1, 4 };   cout << a + b << endl;   cout << a - b << endl;   cout << a * b << endl;   cout << a / b << endl;

--- cpp/test/main.cpp
+++ cpp/test/main.cpp
@@ -1,6 +1,7 @@
 ﻿#include<algorithm>
 #include<base/BaseConverter.h>
 #include<base/Counter.h>
+#include<base/math/Fraction.h>
 #include<ffmpeg-wrapper/mux/SptsEncodeMux.h>
 #include<filesystem>
 #include<QtCore/QCoreApplication>
@@ -17,10 +18,16 @@ int main(void)
 	try
 	{
 		std::filesystem::current_path(Predefine_ResourceDir);
-		test_SptsEncodeMux();
+		//test_SptsEncodeMux();
 		//test_AVPacketPlayer();
 		//test_tsduck();
 		//test_libusb();
+		base::Fraction a { 1, 5 };
+		base::Fraction b { 1, 4 };
+		cout << a + b << endl;
+		cout << a - b << endl;
+		cout << a * b << endl;
+		cout << a / b << endl;
 		return 0;
 	}
 	catch (std::runtime_error &e)
