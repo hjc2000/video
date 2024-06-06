6f893dae00a25aae20a3d24d0b7e8509d6c371d7
hjc2000 <hjc.666@foxmail.com>
2024-06-06 13:27:20 +0800

    //std::filesystem::current_path(Predefine_ResourceDir);

--- cpp/test/main.cpp
+++ cpp/test/main.cpp
@@ -17,7 +17,7 @@ int main(void)
 {
 	try
 	{
-		std::filesystem::current_path(Predefine_ResourceDir);
+		//std::filesystem::current_path(Predefine_ResourceDir);
 		//test_SptsEncodeMux();
 		//test_AVPacketPlayer();
 		//test_tsduck();
