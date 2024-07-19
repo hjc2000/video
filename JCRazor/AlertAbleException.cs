272ae8a3fee3dc82875d7534077ab1b01b7ae27e
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2024-07-20 00:31:18 +0800

    删除JCRazor文件夹

--- JCRazor/AlertAbleException.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace JCRazor;
-
-/// <summary>
-///		此异常的消息会通过 alert 通知用户。这用来告知用户它的操作没有达到想要的结果。
-///		此异常由底层抛出，组件收到后应该 alert。
-/// </summary>
-public class AlertAbleException(string? message) : Exception(message)
-{
-}
